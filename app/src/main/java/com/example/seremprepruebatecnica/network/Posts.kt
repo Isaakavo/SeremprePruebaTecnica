4f37ca52d5da849ae4d7e161717c5d88a0c72690
Isaakavo <isaakhaas96@gmail.com>
2021-05-08 11:20:12 -0500

    Added variables

--- app/src/main/java/com/example/seremprepruebatecnica/network/Posts.kt
+++ app/src/main/java/com/example/seremprepruebatecnica/network/Posts.kt
@@ -1,3 +1,6 @@
 package com.example.seremprepruebatecnica.network
 
-data class Posts(val id: Long, val  body: String, val  title:String, val userId: Long)
+data class Posts(val id: Long, val  body: String, val  title:String, val userId: Long){
+    var isRead = false
+    var isFavorite = false;
+}
